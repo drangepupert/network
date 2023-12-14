bb9f02ba14f0eb4b287012989e0785e21adfb50d
Колесников Александр <sashakoleso0@gmail.com>
2023-12-15 00:25:20 +0300

    Добавьте файлы проекта.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    private NetworkVariable<int> randomnumber = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+
+
+
+    public override void OnNetworkSpawn()
+    {
+        randomnumber.OnValueChanged += (int previousValue, int newValue) =>
+        {
+            Debug.Log(OwnerClientId + "; randomnumber: " + randomnumber.Value);
+        };
+    }
+    private void Update()
+    {
+       
+        if (!IsOwner) return;
+
+        if (Input.GetKeyDown(KeyCode.T)) {
+            randomnumber.Value = Random.Range(0, 100);
+        }
+        Vector3 moveDir = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
+        if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+
+        float moveSpeed = 3f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+}
